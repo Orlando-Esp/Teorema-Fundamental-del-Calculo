174800005b050748c0d888a67479f14ea129d1e9
Orlando-Esp <82452841+Orlando-Esp@users.noreply.github.com>
2021-05-27 15:06:11 -0500

    Update TeoremaFundamentaldelCalculo_Ej4_OrlandoEspinoza_3202.m

--- TeoremaFundamentaldelCalculo_Ej4_OrlandoEspinoza_3202.m
+++ TeoremaFundamentaldelCalculo_Ej4_OrlandoEspinoza_3202.m
@@ -25,7 +25,7 @@ fprintf('Valor de la derivada:%2.1f\n',operacion)
 %Intervalos definidos
 a = 1;
 b = 4;
-n = 100;
+n = 20;
 base = (b-a)/n;
 %Funcion definida
 x =a:base:(b-base);
@@ -33,10 +33,10 @@ x =a:base:(b-base);
 altura=(1+y.^1/2./y.^2);
 %Valor de cada uno de los rectangulos.
 area = base*altura;
-x2 = linspace(1,4,100);
+x2 = linspace(1,4,20);
 y2 = (1 + y.^1/2./y.^2);
 %Fin e inicio de los intervalos
-xa = linspace(1,4,100);
+xa = linspace(1,4,20);
 yab = (1 + y.^1/2./y.^2);
 g = bar(xa,yab,'histc');
 hold on
